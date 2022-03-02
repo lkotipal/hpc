5cff36b9a08cd1557719299bfc0fc39d80856064
lkotipal <leokotipalo@gmail.com>
2022-03-02 19:33:06 +0200

    errors

--- kotipalo_leo_ex06/p04/p04.py
+++ kotipalo_leo_ex06/p04/p04.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import describe
 from scipy.optimize import curve_fit
 
-x = np.linspace(0, 1E8, 51) * 4E-6	# MB
+x = np.linspace(0, 1E8, 51)
 y = np.zeros(51)
 yerr = np.zeros(51)
 for i in range(51):
@@ -11,11 +11,12 @@ for i in range(51):
 	(_, _, y[i], var, _, _) = describe(a)
 	yerr[i] = np.sqrt(var)
 
-(a, b), _ = curve_fit(lambda x, a, b: a * x + b, xdata=x, ydata=y, sigma=yerr)
-print(f'b = {(1/a)} MB/s')
-print(f'l = {b} s')
+(b, l), pcov = curve_fit(lambda x, b, l: x/b + l, xdata=x, ydata=y, sigma=yerr)
+(b_err, l_err) = np.sqrt(np.diag(pcov))
+print(f'b = {b:e} +- {b_err:e} B/s')
+print(f'l = {l} +- {l_err} s')
 plt.errorbar(x, y, yerr, fmt=' ', capsize=2.0)
-plt.plot(x, a * x + b)
-plt.xlabel(f'$N$ (MB)')
+plt.plot(x, x/b + l)
+plt.xlabel(f'$N$ (B)')
 plt.ylabel(f'$t$ (s)')
 plt.savefig('p04.png')
