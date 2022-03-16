55dc326a6c659df59ebb8dd2a89063d489c1fed0
Leo Kotipalo <leokotipalo@gmail.com>
2022-03-16 11:22:15 +0200

    Numerically stable

--- kotipalo_leo_ex07/p03/mpiexample.cpp
+++ kotipalo_leo_ex07/p03/mpiexample.cpp
@@ -34,35 +34,35 @@ int main(int argc,char *argv[])
 			displacements[i] = displacements[i-1] + counts[i-1];
 	}
 
-	int count{0};
-	MPI_Scatter(counts.data(), 1, MPI_INT, &count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	int my_n{0};
+	MPI_Scatter(counts.data(), 1, MPI_INT, &my_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	std::vector<double> nums(count);
-	MPI_Scatterv(file_nums.data(), counts.data(), displacements.data(), MPI_DOUBLE, nums.data(), count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	std::vector<double> my_nums(my_n);
+	MPI_Scatterv(file_nums.data(), counts.data(), displacements.data(), MPI_DOUBLE, my_nums.data(), my_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	double sum{0.0};
-	double square_sum{0.0};
-	for (double d : nums) {
-		sum += d;
-		square_sum += d * d;
+	double my_mean{0.0};
+	double my_square_sum{0.0};
+	for (int i = 0; i < my_n; ++i) {
+		double temp = my_mean;
+		my_mean += (my_nums[i] - my_mean) / (i+1);
+		my_square_sum += (my_nums[i] - temp) * (my_nums[i] - my_mean);
 	}
 
-	std::vector<double> sums(id ? 0 : ntasks);
+	std::vector<double> means(id ? 0 : ntasks);
 	std::vector<double> square_sums(id ? 0 : ntasks);
-	MPI_Gather(&sum, 1, MPI_DOUBLE, sums.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	MPI_Gather(&square_sum, 1, MPI_DOUBLE, square_sums.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Gather(&my_mean, 1, MPI_DOUBLE, means.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Gather(&my_square_sum, 1, MPI_DOUBLE, square_sums.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 	if (id == 0) {
 		for (int i = 1; i < ntasks; ++i) {
-			// Might have numeric instabilities
-			sum += sums[i];
-			square_sum += square_sums[i];
+			double delta = means[i] - my_mean;
+			my_mean += delta * counts[i] / (my_n + counts[i]);
+			my_square_sum += square_sums[i] + delta * delta * (my_n * counts[i]) / (my_n + counts[i]);
+			my_n += counts[i];
 		}
 
-		double mean = sum / file_nums.size();
-		std::cout << "Average:\t" << mean << std::endl;
-		// Population variance, without Bessel correction
-		std::cout << "Variance:\t" << square_sum / file_nums.size() - mean * mean << std::endl;
+		std::cout << "Average:\t" << my_mean << std::endl;
+		std::cout << "Variance:\t" << my_square_sum / my_n << std::endl; // Population variance, without Bessel correction
 	}
 
 	MPI_Finalize();
