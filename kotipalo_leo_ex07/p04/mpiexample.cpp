972c1927aa4d1b09a5a488231a8ce6a5f484d7b0
Leo Kotipalo <leokotipalo@gmail.com>
2022-03-16 17:15:49 +0200

    Cleaner implementation

--- kotipalo_leo_ex07/p04/mpiexample.cpp
+++ kotipalo_leo_ex07/p04/mpiexample.cpp
@@ -1,5 +1,5 @@
+#include <cmath>
 #include <iostream>
-#include <memory>
 #include <mpi.h>
 #include <vector>
 
@@ -14,59 +14,39 @@ int main(int argc,char *argv[])
 		exit(1);
 	}
 	MPI_Comm_size(MPI_COMM_WORLD,&ntasks);
-
 	MPI_Comm_rank(MPI_COMM_WORLD,&id);
 
 	int msg{id};
-	for (int i = ntasks-1; i >= 0; --i) {
-		if (i % 2 == 0) {
-			// Only even IDs gather
-			int count_send{0};
-			int count_receive{0};
-			std::vector<int> count_recv(id == i ? ntasks : 0);
-			std::vector<int> displacements(id == i ? ntasks : 0);
-			if (i == 0) {
-				// Root, gather from all powers of 2
-				if (id == i) {
-					for (int j = 1; j < ntasks; j *= 2) {
-						count_recv[j] = 1;
-						displacements[j] = count_receive++;
-					}
-				} else if ((id & id - 1) == 0) {
-					count_send = 1;
-				}
-			} else {
-				// Gather from next number
-				if (id == i) {
-					count_recv[i+1] = 1;
-					displacements[i+1] = count_receive++;
-				} else if (id == i + 1) {
-					count_send = 1;
-				}
+	int buf{0};
+	if (id == 0) {
+		// Root receives from all powers of 2
+		for (int i = 1; i < ntasks; i *= 2) {
+			MPI_Recv(&buf, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+			msg += buf;
+		}
 
-				if ((i & i - 1) == 0) {
-					// Power of 2, gather from all even numbers up to next power
-					if (id == i) {
-						for (int j = i + 2; j < i * 2 && j < ntasks; j += 2) {
-							count_recv[j] = 1;
-							displacements[j] = count_receive++;
-						}
-					} else if (id % 2 == 0 && id > i && id < 2 * i) {
-						count_send = 1;
-					}
-				}
+		std::cout << msg << std::endl;
+	} else if (id % 2) {
+		// Odd processes send to previous process
+		MPI_Send(&msg, 1, MPI_INT, id - 1, tag, MPI_COMM_WORLD);
+	} else {
+		// Even processes receive from next process, if they're not the last one
+		if (id < ntasks - 1)
+			MPI_Recv(&buf, 1, MPI_INT, id + 1, tag, MPI_COMM_WORLD, &status);
+		msg += buf;
+
+ 		if (id & id - 1) {
+			// If not power of 2, send to previous one
+			MPI_Send(&msg, 1, MPI_INT, std::exp2(static_cast<int>(std::log2(id))), tag, MPI_COMM_WORLD);
+		} else {
+			// If power of 2, receive from even numbers and send to root
+			for (int i = id + 2; i < id * 2 && i < ntasks; i += 2) {
+				MPI_Recv(&buf, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+				msg += buf;
 			}
-			std::vector<int> buffer_recv(count_receive);
-			MPI_Gatherv(&msg, count_send, MPI_INT, buffer_recv.data(), count_recv.data(), displacements.data(), MPI_INT, i, MPI_COMM_WORLD);
-
-			if (id == i)
-				for (int j = 0; j < buffer_recv.size(); ++j)
-					msg += buffer_recv[j];
+			MPI_Send(&msg, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
 		}
 	}
-
-	if (id == 0)
-		std::cout << msg << std::endl;
 
 	MPI_Finalize();
 	exit(0);
